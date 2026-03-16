3c547f32cd804510ee8cf0a7f20544a8ffb210c8
Jhonnathas <jhonnathas.santos@ccc.ufcg.edu.br>
2026-03-16 17:36:29 -0300

    igest

---
+++ ingestion/ingest.py
@@ -0,0 +1,23 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from langchain_openai import OpenAIEmbeddings
+
+loader = PyPDFLoader("data/saude_publica.pdf")
+
+docs = loader.load()
+
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size=500,
+    chunk_overlap=50
+)
+
+chunks = splitter.split_documents(docs)
+
+embeddings = OpenAIEmbeddings()
+
+vectorstore = FAISS.from_documents(chunks, embeddings)
+
+vectorstore.save_local("vectorstore")
+
+print("Documentos indexados com sucesso")
